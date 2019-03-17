7a070ffa51322f1e74b49400d2cca554e4e7dd03
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-03-18 01:26:13 +0900

    0318 올림

---
+++ 연습모음사이트/_index.js
@@ -0,0 +1,109 @@
+function code_0717(){
+  location.href="july/0717_code.html"
+}
+function code_0718_1(){
+  location.href="july/0718_1_code.html"
+}
+function code_0718_2(){
+  location.href="july/0718_2_code.html"
+}
+function code_0718_3(){
+  location.href="july/0718_3_code.html"
+}
+function code_0719(){
+  location.href="july/0719_code.html"
+}
+function code_0723(){
+  location.href="july/0723_code.html"
+}
+function code_0724(){
+  location.href="july/0724_code.html"
+}
+function code_0725(){
+  location.href="july/0725_code.html"
+}
+function code_0731(){
+  location.href="july/0731_code.html"
+}
+
+<!--      --------------------------구분선---------------------------------       -->
+function code_0814(){
+  location.href="august/0814_code.html"
+}
+function code_0817(){
+  location.href="august/0817_code.html"
+}
+function code_0819(){
+  location.href="august/0819_code.html"
+}
+function code_0820(){
+  location.href="august/0820_code.html"
+}
+function code_0821(){
+  location.href="august/0821_1_code.html"
+}
+function code_0821_2(){
+  location.href="august/0821_2_code.html"
+}
+function code_0821_3(){
+  location.href="august/0821_3_code.html"
+}
+<!--      --------------------------구분선---------------------------------       -->
+function code_0904(){
+  location.href="september/0904_code.html"
+}
+function code_0907(){
+  location.href="september/0907_code.html"
+}
+function code_0908(){
+  location.href="september/0908_code.html"
+}
+function code_0918(){
+  location.href="september/0918_code.html"
+}
+function code_0925(){
+  location.href="september/0925_code.html"
+}
+function code_0929(){
+  location.href="september/0929_code.html"
+}
+function code_0930_1(){
+  location.href="september/0930_1_code.html"
+}
+function code_0930_2(){
+  location.href="september/0930_2_code.html"
+}
+<!--      --------------------------구분선---------------------------------       -->
+function code_1001(){
+  location.href="october/1001_code.html"
+}
+function code_1028(){
+  location.href="october/1028_code.html"
+}
+function code_1029_1(){
+  location.href="october/1029_1_code.html"
+}
+function code_1029_2(){
+  location.href="october/1029_2_code.html"
+}
+function code_1029_3(){
+  location.href="october/1029_3_code.html"
+}
+function code_1029_4(){
+  location.href="october/1029_4_code.html"
+}
+function code_1030_1(){
+  location.href="october/1030_1_code.html"
+}
+function code_1030_2(){
+  location.href="october/1030_2_code.html"
+}
+function code_1031_1(){
+  location.href="october/1031_1_code.html"
+}
+function code_1031_2(){
+  location.href="october/1031_2_code.html"
+}
+function code_1031_3(){
+  location.href="october/1031_3_code.html"
+}
